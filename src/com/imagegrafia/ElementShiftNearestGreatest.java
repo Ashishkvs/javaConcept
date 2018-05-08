93d5b4b245740968bda62ab2dc348899da3ecee7
Ashish Yadhuvanshi <Ashishkvs@gmail.com>
2018-05-08 10:51:12 +0530

    ArrayElement shift to nearest greatest in the array

---
+++ src/com/imagegrafia/ElementShiftNearestGreatest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class ElementShiftNearestGreatest {
+	public static void main(String[] args) {
+		Integer a[] = { 10, 8, 13, 16, 15, 9, 14, 11 };
+
+		for (int i = 0; i < a.length; i++) {
+			int j=i+1;
+			while (j < a.length) {
+				if (a[i] < a[j]) {
+					a[i]=a[j];
+					break;
+				}
+				j++;
+			}
+		}
+
+		for (Integer k : a) {
+			System.out.println(k);
+		}
+	}
+
+}
